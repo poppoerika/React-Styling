fdc57a185eafdeb11eda7cd7bb673837e99451f8
Erika Tharp <eems0926@gmail.com>
2020-09-01 17:57:00 -0700

    Complete the React Styling challenge

--- src/index.js
+++ src/index.js
@@ -5,3 +5,32 @@
 //Apply the "heading" style in the styles.css
 //Dynamically change the color of the h1 using inline css styles.
 //Morning = red, Afternoon = green, Night = blue.
+import React from "react";
+import ReactDOM from "react-dom";
+
+const date = new Date();
+const hours = date.getHours();
+let greeting;
+let greetingColor;
+
+if (hours >= 0 && hours < 12) {
+  greeting = "Good morning";
+  greetingColor = "red";
+} else if (hours >= 12 && hours < 18) {
+  greeting = "Good Afternoon";
+  greetingColor = "green";
+} else if (hours >= 18 && hours <= 23) {
+  greeting = "Good Evening";
+  greetingColor = "blue";
+}
+
+let customStyle = {
+  color: greetingColor
+};
+
+ReactDOM.render(
+  <h1 className="heading" style={customStyle}>
+    {greeting}
+  </h1>,
+  document.getElementById("root")
+);
